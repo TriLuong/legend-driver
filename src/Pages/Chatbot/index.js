b5d09f31863bf273ea5ebd2a5104113f4bc709e8
Tri Luong <triluongdl@gmail.com>
2019-10-31 17:33:46 +0700

    Add uriLogin when error Unauthentication occurs in Chatbot.

--- src/Pages/Chatbot/index.js
+++ src/Pages/Chatbot/index.js
@@ -4,13 +4,14 @@ import { getTokenSelector } from 'Store/auth/selectors'
 import './styles.scss'
 
 const sourceChatbot = 'https://landbot.io/u/H-291303-WLRCM6ZR74RVGHGG/index.html'
+const uriLogin = 'http://localhost:3000/login'
 
 const Chatbot = props => {
   const [isLoading, setIsLoading] = useState(true)
   const { match } = props
   const { loadId } = match.params
   const token = useSelector(getTokenSelector)
-  const srcIframe = `${sourceChatbot}?token=${token}&load_id=${loadId}`
+  const srcIframe = `${sourceChatbot}?token=${token}&load_id=${loadId}&uri_login=${uriLogin}`
 
   const onLoad = () => {
     setIsLoading(false)
