53eb5914c9f786a99124fe60c48b9ba132859397
Tri Luong <triluongdl@gmail.com>
2019-10-31 13:31:41 +0700

    Add chatbot.

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 import Login from 'Pages/Login'
 import LoadHistory from 'Pages/LoadHistory'
 import LoadDetail from 'Pages/LoadDetail'
+import Chatbot from 'Pages/Chatbot'
 // import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={LoadHistory} />
         <Route path="/login" component={Login} />
         <Route path="/loadDetail" component={LoadDetail} />
+        <Route path="/chatbot" component={Chatbot} />
         <Route path="/error" />
       </Switch>
     </Layout>
---
+++ src/Pages/Chatbot/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.scss'
+
+const Chatbot = () => (
+  <div>
+    <iframe
+      title="Chatbot"
+      src="https://landbot.io/u/H-291303-WLRCM6ZR74RVGHGG/index.html"
+      className="chatbot w-100 vh-100"
+    />
+  </div>
+)
+
+export default Chatbot
