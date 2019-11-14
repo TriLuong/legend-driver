9fab815706aea466176ba8135c25935944250b5b
Tri Luong <triluongdl@gmail.com>
2019-11-14 16:58:02 +0700

    Add status of load.

--- src/Components/DataList/index.js
+++ src/Components/DataList/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Badge } from 'reactstrap'
 import moment from 'moment'
 import Assets from 'Assets'
 import './styles.scss'
@@ -9,14 +10,40 @@ const DataList = ({ data }) => {
   const time = moment(data.appointmentTime).format('HH:mm')
   const containerType = data.containerType || 'None'
 
+  const checkStatus = status => {
+    switch (status) {
+      case 'PickedUpNotDelivered': {
+        const text = 'Picked Up'
+        const color = 'var(--picked-up)'
+        return { text, color }
+      }
+      case 'Delivered': {
+        const text = 'Delivered'
+        const color = 'var(--delivered-load)'
+        return { text, color }
+      }
+      default: {
+        const text = 'New'
+        const color = 'var(--new-load)'
+        return { text, color }
+      }
+    }
+  }
+
+  const status = checkStatus(data.status)
   return (
     <Link to={`/loadDetail/${data.id}`} className="row loadItemLink">
       <div className="col-2">
         <img src={Assets.images.loadLeftIcon} />
       </div>
-      <div className="col-10">
+      <div className="col-7">
         <h4 className="loadId">{`#${data.reservation}`}</h4>
         <p className="loadDetail">{`${containerType} • ${date} • ${time}`}</p>
+      </div>
+      <div className="col-3 statusContainer">
+        <p className="loadDetail statusText" style={{ color: status.color }}>
+          {status.text}
+        </p>
       </div>
     </Link>
   )
