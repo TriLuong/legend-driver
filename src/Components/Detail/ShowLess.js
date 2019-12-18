7d0e020552154a098e7963c998490a527252f136
Tri Luong <triluongdl@gmail.com>
2019-12-18 23:55:33 +0700

    Update UI for Load Detail.

--- src/Components/Detail/ShowLess.js
+++ src/Components/Detail/ShowLess.js
@@ -13,52 +13,86 @@ const ShowLess = ({ data }) => {
   const locationPickUp = data.pickUpLocation || 'None'
   return (
     <div className="contentLoad">
-      <div className="contentSection d-flex justify-content-between align-items-center">
-        <p className="contentLoadId">{`#${data.load}`}</p>
+      <div className="section">
+        <div className="contentSection d-flex justify-content-between align-items-center">
+          <p className="contentLoadId">{`#${data.load}`}</p>
+          <IconCircle src={Assets.images.phoneIcon} />
+        </div>
+        <div className="contentSection ">
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextLoadTitle text="Type" />
+            <Badge color="primary" className="loadType">
+              {containerPieceType}
+            </Badge>
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextLoadTitle text="Date" />
+            <TextSub text={date} />
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextLoadTitle text="Time" />
+            <TextSub text={time} />
+          </div>
+        </div>
+      </div>
 
-        <IconCircle src={Assets.images.phoneIcon} />
-      </div>
-      <div className="contentSection ">
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextLoadTitle text="Type" />
-          <Badge color="primary" className="loadType">
-            {containerPieceType}
-          </Badge>
+      <div className="section">
+        <div className="contentSection ">
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextLoadTitle text="Pick-up" />
+            <IconCircle src={Assets.images.navigatorIcon} />
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center pb-0">
+            <TextSub text={locationPickUp} className="loadAddress" />
+          </div>
         </div>
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextLoadTitle text="Date" />
-          <TextSub text={date} />
-        </div>
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextLoadTitle text="Time" />
-          <TextSub text={time} />
-        </div>
-      </div>
-      <div className="contentSection ">
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextLoadTitle text="Pick-up location" />
-          <IconCircle src={Assets.images.navigatorIcon} />
-        </div>
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextSub text={locationPickUp} className="loadAddress" />
+        <div className="contentSection ">
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextSub text="Pick Up #" />
+            <TextSub text={data.pickUp ? data.pickUp : 'None'} />
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextSub text="Appointment #" />
+            <TextSub text={data.appointment} />
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextSub text="Reservation" />
+            <TextSub text={data.reservation} />
+          </div>
         </div>
       </div>
-      <div className="contentSection ">
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextLoadTitle text="Drop-off location" />
-          <IconCircle src={Assets.images.navigatorIcon} />
+
+      <div className="section">
+        <div className="contentSection ">
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextLoadTitle text="Drop-off" />
+            <IconCircle src={Assets.images.navigatorIcon} />
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center pb-0">
+            <TextSub text={locationDropOff} className="loadAddress" />
+          </div>
         </div>
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextSub text={locationDropOff} className="loadAddress" />
+        <div className="contentSection ">
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextSub text="Container #" />
+            <TextSub text={data.chat.container || 'None'} />
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextSub text="Rail Pick Up #" />
+            <TextSub text={data.railPickUp || 'None'} />
+          </div>
         </div>
       </div>
-      <div className="contentSection ">
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextLoadTitle text="Notes" />
-          {/* <IconCircle src={Assets.images.navigatorIcon} /> */}
-        </div>
-        <div className="sectionItem d-flex justify-content-between align-items-center">
-          <TextSub text={data.notes || 'No notes for this load'} className="loadAddress" />
+
+      <div className="section">
+        <div className="contentSection ">
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextLoadTitle text="Notes" />
+            {/* <IconCircle src={Assets.images.navigatorIcon} /> */}
+          </div>
+          <div className="sectionItem d-flex justify-content-between align-items-center">
+            <TextSub text={data.notes || 'No notes for this load'} className="loadAddress" />
+          </div>
         </div>
       </div>
     </div>
