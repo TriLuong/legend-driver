4983274527ceafda9561ea00c5e60fef7f6a1fe3
Tri Luong <triluongdl@gmail.com>
2019-12-20 10:54:36 +0700

    Edit history chatbot to match with new update UI.

--- src/Components/ChatbotHistory/ChatbotDropOff.js
+++ src/Components/ChatbotHistory/ChatbotDropOff.js
@@ -5,15 +5,23 @@ import './landbot-2.2.0.css'
 
 const ChatbotDropOff = ({ data }) => {
   const loadChat = data.chat
-  const imgSrc = loadChat.podImageUrl
+  const imgsSrc = loadChat.podImageUrls
 
   return (
     <ChatbotContainer>
       <BotReview question="Hi there!" isVisible={false} />
       <BotReview question="Welcome to our site.🤗" answer="Hi" />
       <BotReview question="Picking Up or Dropping Off" answer="Dropping Off" />
-      <BotReview question="Delivery # ?" answer={loadChat.delivery} />
-      <BotReview question="Upload the POD" imageSrc={imgSrc} />
+      {/* <BotReview question="Delivery # ?" answer={loadChat.delivery} /> */}
+      {imgsSrc.map((imgSrc, index) => (
+        <>
+          <BotReview key={imgSrc} question="Upload the POD" imageSrc={imgSrc} />
+          <BotReview
+            question="Do you want to upload more POD image?"
+            answer={index === imgsSrc.length - 1 ? 'No' : 'Yes'}
+          />
+        </>
+      ))}
       <BotReview question="Mission Accomplised!!!" />
     </ChatbotContainer>
   )
--- src/Components/ChatbotHistory/ChatbotPickUp.js
+++ src/Components/ChatbotHistory/ChatbotPickUp.js
@@ -5,7 +5,7 @@ import './landbot-2.2.0.css'
 
 const ChatbotPickUp = ({ data }) => {
   const loadChat = data.chat
-  const imgSrc = loadChat.bolImageUrl
+  const imgsSrc = loadChat.bolImageUrls
   let containerType = ''
   if (loadChat.isDropAndHookLoad === 1) {
     if (loadChat.containerType === 'LoadDropped') {
@@ -19,7 +19,6 @@ const ChatbotPickUp = ({ data }) => {
       <BotReview question="Hi there!" isVisible={false} />
       <BotReview question="Welcome to our site.🤗" answer="Hi" />
       <BotReview question="Picking Up or Dropping Off" answer="Picking Up" />
-      <BotReview question="Upload the BOL" imageSrc={imgSrc} />
       <BotReview
         question="Is this a Drop and Hook load?"
         answer={loadChat.isDropAndHookLoad ? 'Yes' : 'No'}
@@ -28,9 +27,18 @@ const ChatbotPickUp = ({ data }) => {
         <BotReview question="What is Container 1#?" answer={containerType} />
       )}
       <BotReview question="Container # ?" answer={loadChat.container} />
+      {imgsSrc.map((imgSrc, index) => (
+        <>
+          <BotReview key={imgSrc} question="Upload the BOL" imageSrc={imgSrc} />
+          <BotReview
+            question="Do you want to upload more BOL image?"
+            answer={index === imgsSrc.length - 1 ? 'No' : 'Yes'}
+          />
+        </>
+      ))}
       <BotReview question="Chassis # ?" answer={loadChat.chassis} />
       <BotReview question="Piece Count ?" answer={loadChat.containerPieceCount} />
-      <BotReview question="Piece ?" answer={loadChat.containerPiece} />
+      {/* <BotReview question="Piece ?" answer={loadChat.containerPiece} /> */}
       <BotReview question="Piece Type ?" answer={loadChat.containerPieceType} />
       <BotReview question="Weight (lbs) ?" answer={loadChat.containerWeight} />
       <BotReview question="Seal ?" answer={loadChat.containerSealNumber} />
